ded42e8a271870415170275ed65d9164440cbacb
zhangjie <me@zhangjiee.com>
2018-11-17 16:32:27 +0800

    category anchor jump

--- src/src/pages/Blog/BlogCategory.js
+++ src/src/pages/Blog/BlogCategory.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'dva';
 import { Spin } from 'antd';
 import Link from 'umi/link';
-
+import { goToAnchor } from 'react-scrollable-anchor';
 import styles from './Blog.less';
 
 @connect(({ blog, loading }) => ({
@@ -18,6 +18,16 @@ class BlogCategory extends Component {
     });
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.blog.categories !== prevProps.blog.categories) {
+      let { hash } = this.props.location;
+      hash = hash.slice(1);
+      if (hash.length !== 0) {
+        goToAnchor(hash);
+      }
+    }
+  }
+
   render() {
     const {
       blog: { categories },
@@ -31,7 +41,7 @@ class BlogCategory extends Component {
             {categories.map(item => {
               return (
                 <div key={item.category.slug} className={styles.archiveRow}>
-                  <h2 name={item.category.slug}>{item.category.name}</h2>
+                  <h2 id={item.category.slug}>{item.category.name}</h2>
                   <ul>
                     {item.articles.map(article => {
                       return (
